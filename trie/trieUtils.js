4a64f0dbcf09404dab8fbbc97e8d2ef033cdd593
LiorKirshner <liorkk7@gmail.com>
2025-07-24 18:28:13 +0300

    printAllLetters

---
+++ trie/trieUtils.js
@@ -0,0 +1,15 @@
+//const AutoCompleteTrie = require("./AutoCompleteTrie");
+
+const printAllLetters = function (trie) {
+  function traverse(node, indent = "") {
+    if (node.value) {
+      console.log(indent + node.value + (node.endOfWord ? " *" : ""));
+    }
+    for (let char in node.children) {
+      traverse(node.children[char], indent + "  ");
+    }
+  }
+  traverse(trie.root);
+};
+
+module.exports = printAllLetters;
